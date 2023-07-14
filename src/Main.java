14a1f983d16d31cdc96702cfef94407af08be4e9
DanielBelovol <danielbelovol08@gmail.com>
2023-07-14 21:11:27 +0300

    text 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello my FRIENDS!!");
+        System.out.println("Hello my fRIENDS!!");
 
         System.out.println("This is the test");
     }
